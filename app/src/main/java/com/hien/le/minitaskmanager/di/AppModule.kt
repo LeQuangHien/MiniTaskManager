c2e29d1133c3faa118eb6df6e9dbdd6079a9e818
Hien Le <lequanghien247@gmail.com>
2025-04-29 14:55:42 +0200

    Configure Room database and add TaskRepository: - Create Room database instance, including providing the `AppDatabase` implementation and the `TaskDao`. - Create `TaskRepository` with `TaskDao` dependency. - Inject `TaskRepository` dependency to `TaskViewModel`.

--- app/src/main/java/com/hien/le/minitaskmanager/di/AppModule.kt
+++ app/src/main/java/com/hien/le/minitaskmanager/di/AppModule.kt
@@ -1,9 +1,18 @@
 package com.hien.le.minitaskmanager.di
 
+import androidx.room.Room
+import com.hien.le.minitaskmanager.data.datasource.database.AppDatabase
+import com.hien.le.minitaskmanager.data.repository.TaskRepository
 import com.hien.le.minitaskmanager.ui.screens.task.TaskViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
-    viewModel { TaskViewModel() }
+    single<AppDatabase> {
+        Room.databaseBuilder(get(), AppDatabase::class.java, "mtm_database").build()
+    }
+    single { get<AppDatabase>().taskDao() }
+    single { TaskRepository(get()) }
+
+    viewModel { TaskViewModel(get()) }
 }
