6882750eded7d175f7fae938d42e8f6ff87426f1
Hien Le <lequanghien247@gmail.com>
2025-04-29 10:17:13 +0200

    Enable KSP (Kotlin Symbol Processing) in the project by: - Adding the KSP plugin to the `libs.versions.toml` file. - Applying the KSP plugin in the root `build.gradle.kts` file. - Applying the KSP plugin in the app's `build.gradle.kts` file.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -52,7 +53,7 @@ dependencies {
 
     implementation(libs.koin.android)
     implementation(libs.koin.compose)
-    
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
